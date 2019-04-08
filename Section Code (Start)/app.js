bcd428afed7015f806b006a077deba8ce2571297
manuelmarcas <manumarcas@usal.es>
2019-04-08 14:24:32 +0200

    clase 72

---
+++ Section Code (Start)/app.js
@@ -0,0 +1,63 @@
+
+var data = {
+  title: 'The VueJS Instance',
+  showParagraph: false
+}
+
+
+var vm1 = new Vue({
+  el: '#app1',
+  data: data,
+  methods: {
+    show: function() {
+      this.showParagraph = true;
+      this.updateTitle('The VueJS Instance (Updated)');
+      // console.log(this.$refs);
+      this.$refs.myButton.innerText = 'test';
+    },
+    updateTitle: function(title) {
+      this.title = title;
+    }
+  },
+  computed: {
+    lowercaseTitle: function() {
+      return this.title.toLowerCase();
+    }
+  },
+  watch: {
+    title: function(value) {
+      alert('Title changed, new value: ' + value);
+    }
+  }
+});
+
+vm1.$mount('#app1');
+
+console.log(vm1.$data ===  data);
+vm1.$refs.heading.inneText = 'Something else';
+
+setTimeout(function() {
+  vm1.title = 'Changed by Timer';
+}, 3000)
+
+
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'The second Instance'
+  },
+  methods: {
+    onChange: function() {
+      vm1.title = 'Changed!';
+    }
+  }
+});
+
+
+var vm3 = new Vue({
+  el: 'hello',
+  template: '<h1>Hello!</h1>'
+});
+
+// vm3.$mount('#app3');
+// document.getElementById('app3').appendChild(vm3.$el);
--- Setup/Setup/app.js
+++ Setup/Setup/app.js
@@ -3,56 +3,110 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
+
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true, 
+                text: 'Player hits Monster for ' + damage
+            });
             
-            this.monsterHealth -= this.calculateDamage(3, 10);
-            if(this.checkWin()){
+
+            let state = this.checkWin();
+            console.log(state);
+            if(true == state){
+                console.log('SI');
                 return;
             }
 
-            this.playerHealth -= this.calculateDamage(3, 10);
-            if(this.checkWin()){
-                return;
-            }
-            
+            this.monsterAttacks(7, 12);
         },
         specialAttack: function() {
-            
+
+            var damage = this.calculateDamage(7, 15);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true, 
+                text: 'Player hits Monster hard for ' + damage
+            });
+            if(this.checkWin()){
+                return;
+            }else{
+                this.monsterAttacks(10, 20);
+            }
+ 
         },
         heal: function() {
-            
+
+            if(this.playerHealth <= 90){
+                this.playerHealth += 10;
+            }else{
+                this.playerHealth = 100;
+            }
+
+            this.turns.unshift({
+                isPlayer: true, 
+                text: 'Player heals for 10'
+            });
+            this.monsterAttacks(7, 12);
+
+        },
+        monsterAttacks: function(min, max) {
+
+            var damage = this.calculateDamage(min, max);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false, 
+                text: 'Monster hits player for ' + damage
+            });
+
         },
         giveUp: function() {
-            
+            this.gameIsRunning = false;
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) +1, min);
         },
         checkWin: function() {
+
             if(this.monsterHealth <= 0){
+                
                 if(confirm('You WON! New Game?')){
                     this.startGame();
                 }else{
-                    gameIsRunning = false;
+                    this.gameIsRunning = false;
                 }
-                return;
+
+                console.log('hemos ganado');
+
+                return true;
+
             }else if(this.playerHealth <= 0){
+
                 if(confirm('You LOST! New Game?')){
                     this.startGame();
                 }else{
-                    gameIsRunning = false;
+                    this.gameIsRunning = false;
                 }
-                return;
+
+        
+                console.log('hemos perdido');
+                return true;
             }
             return false;
+
         }
     }
 
