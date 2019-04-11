b1a9b6edf7d35b1cef57cb53b501e5e5cb64f2e7
manuelmarcas <manumarcas@usal.es>
2019-04-11 14:29:50 +0200

    save

---
+++ clase 117/Section Code (Start)/src/components/New.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>The Quote</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        data: function() {
+            return {
+                counter: 0
+            };
+        }
+    }
+</script>
+<style scoped>
+
+</style>
