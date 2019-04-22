f096e5a6313dfa1017b05b3bf94f81b091b054f9
manuelmarcas <manumarcas@usal.es>
2019-04-22 14:20:06 +0200

    clase 213

--- clase 183/src/App.vue
+++ clase 183/src/App.vue
@@ -4,25 +4,167 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+
+                <br>
                 <button class="btn btn-primary" @click="show = !show">Show alert</button>
+                <br><br><br>
+
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                
+                </transition>
+                <transition name="slide" type="animation">
+                    <div class="alert alert-info" v-if="show">Slide</div>
+                </transition>
+
+
+                <!-- SE ANIMA AL RECARGAR LA PAGINA -->
+                <transition name="fade" appear>
+                    <div class="alert alert-info" v-if="show">Fade appear</div>
+                </transition>
                 <br><br>
-                <transition name="fade">
-                    <div class="alert alert-info" v-if="show">This is some info</div>
+
+
+                <!-- ANIMATE.CSS -->
+                <transition 
+                    appear
+                    enter-active-class="animated swing"
+                    leave-active-class="animated wobble">
+                        <div class="alert alert-info" v-if="show">swing & wobble</div>
                 </transition>
-                <transition name="slide">
-                    <div class="alert alert-info" v-if="show">This is some info</div>
+                <br><br>
+
+
+                <!-- ANIMACION ENTRE DOS OBJETOS -->
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+                    <div class="alert alert-warning" v-if="!show" key="warning">This is some warning</div>
                 </transition>
+
+                <hr>
+
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br><br>
+                <transition 
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    @enter-cancelled="enterCancelled"
+                    
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+                    :css="false">
+                        <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
+                </transition>
+            
+                <hr>
+                <button class="btn btn-primary"
+                    @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'">
+                    Toogle Components
+                </button>
+                <br><br>
+               <transition name="fade" mode="out-in">
+                    <component :is="selectedComponent"></component>
+                </transition>
+
+                <hr>
+                <button class="btn -primary" @click="addItem">Add Item</button>
+                <ul class="list-group">
+                    <transition-group name="slide">
+                        <li 
+                            class="list-group-item" 
+                            v-for="(number, index) in numbers" 
+                            @click="removeItem(index)"
+                            style="cursor: pointer"
+                            :key="number">
+                                {{ number }}
+                        </li>
+                    </transition-group>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import DangerAlert from './DangerAlert.vue';
+    import SuccessAlert from './SuccessAlert.vue';
+
     export default {
         data() {
             return {
-                show: false
+                show: false,
+                load: true,
+                alertAnimation: 'fade',
+                elementWidth: 100,
+                selectedComponent: 'app-success-alert',
+                numbers: [1, 2, 3, 4, 5]
             }
+        }, 
+        methods: {
+            beforeEnter(el) {
+                console.log('beforeEnter');
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
+            },
+            enter(el, done) {
+                console.log('enter');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth + round * 10) + 'px';
+                    round++;
+                    if(round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
+            },
+            afterEnter(el) {
+                console.log('afterEnter');
+            },
+            enterCancelled(el) {
+                console.log('enterCancelled');
+            },
+            beforeLeave(el) {
+                console.log('beforeLeave');
+                this.elementWidth = 300;
+                el.style.width = this.elementWidth + 'px';
+            },
+            leave(el, done) {
+                console.log('leave');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth - round * 10) + 'px';
+                    round++;
+                    if(round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
+            },
+            afterLeave(el) {
+                console.log('afterLeave');
+            },
+            leaveCancelled(el) {
+                console.log('leaveCancelled');
+            },
+            addItem() {
+                const pos = Math.floor(Math.random() * this.numbers.length);
+                this.numbers.splice(pos, 0, this.numbers.length + 1);
+            },
+            removeItem(index) {
+                this.numbers.splice(index, 1);
+            }
+        },
+        components: {
+            appDangerAlert: DangerAlert,
+            appSuccessAlert: SuccessAlert
         }
     }
 </script>
@@ -34,7 +176,7 @@
 
     .fade-enter-active {
         transition: opacity 1s;
-        /* opacity: 1; */
+        /*opacity: 0;*/
     }
 
     .fade-leave {
@@ -46,12 +188,14 @@
         opacity: 0;
     }
 
-    .slide-enter {
 
+    .slide-enter {
+        opacity: 0;
     }
 
     .slide-enter-active {
         animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
     }
 
     .slide-leave {
@@ -59,7 +203,14 @@
     }
 
     .slide-leave-active {
-        animation: slide-out;
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
+        position: absolute;
+    }
+
+    .slide-move {
+        transition: transform 1s;
     }
 
     @keyframes slide-in {
